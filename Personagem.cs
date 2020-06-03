efd4ad6d3d4e45da459cab0bcd5c8b99225206d3
LuannaCorreia <lusinha995@gmail.com>
2020-06-03 15:51:51 -0300

    Aula 3 finalizada

---
+++ Personagem.cs
@@ -0,0 +1,16 @@
+namespace Aula3POOHerois
+{
+    public class Personagem
+    {
+        //Atributos
+        public string nome;
+        public int idade;
+        public string armadura;
+
+        //Método
+        public string Atacar(){
+            return "O personagem atacou!";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aula3POOHerois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Personagem Tony = new Personagem();
+            Tony.armadura = "Bleeding edge";
+
+            Console.WriteLine(Tony.nome);
+            Console.WriteLine(Tony.armadura);
+            Console.WriteLine(Tony.Atacar() );
+        }
+    }
+}
